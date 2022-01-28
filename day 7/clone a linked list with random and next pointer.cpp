54a261fbb150b73bc02ff045321978a97aca02c3
up1512001 <utsav.patel1215@outlook.com>
2022-01-28 23:17:49 +0530

    day 7 completed...

--- day 7/clone a linked list with random and next pointer.cpp
+++ day 7/clone a linked list with random and next pointer.cpp
@@ -4,6 +4,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// brute force will be O(N + N) and O(N)
+//  create unordered_map<int,pair<Node*,Node*>> mp;
+
+
 /*
 // Definition for a Node.
 class Node {
@@ -78,4 +82,79 @@ public:
 
 
 
+// other approach
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
 
+class Solution
+{
+public:
+    void dfs(Node *head)
+    {
+        Node *p = head;
+        while (p != nullptr)
+        {
+            cout << p->val << " ";
+            p = p->next;
+        }
+    }
+    Node *copyRandomList(Node *head)
+    {
+        if (!head)
+            return head;
+
+        Node *temp = head;
+
+        //creates mapping of cloned list using original list
+        while (temp)
+        {
+            Node *newNode = new Node(temp->val);
+            newNode->next = temp->next;
+            temp->next = newNode;
+            temp = newNode->next;
+        }
+
+        //assigning random pointers
+        temp = head;
+        while (temp)
+        {
+            if (temp->random)
+            {
+                temp->next->random = temp->random->next;
+            }
+            temp = temp->next->next;
+        }
+
+        //restore the original linked list
+        Node *newHead = head->next;
+        Node *temp2 = head->next;
+        temp = head;
+
+        while (temp && temp2)
+        {
+            Node *currN = temp2->next;
+            temp->next = currN;
+            if (currN)
+            {
+                temp2->next = currN->next;
+            }
+            temp = temp->next;
+            temp2 = temp2->next;
+        }
+        // dfs(newHead);
+        return newHead;
+    }
+};
