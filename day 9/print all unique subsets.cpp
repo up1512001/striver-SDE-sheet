6510d2a7e3a5fb07eaf71a3e996b619d8c78b430
up1512001 <utsav.patel1215@outlook.com>
2022-02-01 00:05:52 +0530

    updated unique subset problem

--- day 9/print all unique subsets.cpp
+++ day 9/print all unique subsets.cpp
@@ -34,3 +34,34 @@ int main(){
     }
 
 }
+
+/*
+
+// 0ms runtime leetcode
+
+class Solution {
+public:
+    
+    void f(int ind,vector<int>&nums,vector<vector<int>> &ans,vector<int> &temp){
+        ans.push_back(temp);
+        for(int i=ind;i<nums.size();i++){
+            if(i > ind && nums[i]==nums[i-1]) continue;
+            temp.push_back(nums[i]);
+            f(i+1,nums,ans,temp);
+            temp.pop_back();
+            // f(i+1,nums,ans,temp);
+        }
+    }
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        sort(nums.begin(),nums.end());
+        f(0,nums,ans,temp);
+        return ans;
+    }
+};
+
+
+
+*/
