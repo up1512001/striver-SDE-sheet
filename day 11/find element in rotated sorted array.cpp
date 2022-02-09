b0c5cf95c39aa5520f1d5eb980bc2a60b277152f
up1512001 <utsav.patel1215@outlook.com>
2022-02-09 23:49:05 +0530

    day 11 edited

--- day 11/find element in rotated sorted array.cpp
+++ day 11/find element in rotated sorted array.cpp
@@ -13,11 +13,31 @@ int BinarySearchRotatedSorted(vector<int> &arr,int key){
             if(arr[mid]>key && arr[l]<=key) r= mid-1;
             else l = mid+1;
         }else{
-            if(arr[mid]<key && arr[r]<=key) l=mid+1;
+            if(arr[mid]<key && arr[r]>=key) l=mid+1;
             else r=mid-1;
         }
     }
     return -1;
+}
+
+// find pivot where it is rotated
+int pivot(vector<int> &nums){
+    int l=0,r=nums.size()-1;
+        if(nums[l]<=nums[r]) return nums[0];
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            int next = (mid+1)%nums.size();
+            int pre = (mid + nums.size()-1)%nums.size();
+            if(nums[mid] <= nums[pre] && nums[mid] <= nums[next]){
+                return nums[mid];
+            }
+            if(nums[0]<=nums[mid]){
+                l=mid+1;
+            }else{
+                r=mid-1;
+            }
+        }
+        return -1;
 }
 
 int main(){
@@ -29,5 +49,5 @@ int main(){
     int k;cin>>k;
 
     cout<<"given value present at :"<<BinarySearchRotatedSorted(arr,k)<<"\n";
-
+    cout<<"number of times array rotated :"<<pivot(arr)<<"\n";
 }
